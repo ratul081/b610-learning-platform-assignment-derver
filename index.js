9f3c7f24705ee77e8586e430b1dc5a96f11ee23e
ratul081 <ratullinux77@gmail.com>
2023-08-25 16:44:54 +0600

    Server deplay Done

---
+++ index.js
@@ -0,0 +1,55 @@
+const express = require('express')
+const app = express()
+const port = process.env.PORT || 5000;
+const cors = require('cors')
+
+app.use(cors())
+
+const categories = require('./data/categories.json')
+const courses = require('./data/courses.json')
+
+app.get('/', (req, res) => {
+  res.send('Hello boys')
+})
+app.get('/courses', (req, res) => {
+  res.send(courses)
+})
+
+app.get('/home', (req, res) => {
+  const shortenCourse = courses.slice(2) 
+  res.send(shortenCourse)
+})
+app.get('/courses', (req, res) => {
+  res.send(courses)
+})
+
+app.get('/courses/:id', (req, res) => {
+  const id = req.params.id
+  const selectedCourse = courses.find(course => course._id === id)
+  res.send(selectedCourse);
+})
+
+app.get('/check_out/:id', (req, res) => {
+  const id = req.params.id
+  const selectedCourse = courses.find(course => course._id === id)
+  res.send(selectedCourse);
+})
+
+app.get('/course-categories', (req, res) => {
+  res.send(categories)
+})
+
+app.get('/category/:id', (req, res) => {
+  const id = req.params.id
+  if (id == '07') {
+    res.send(courses)
+  }
+  else {
+    const selectedCategories = courses.filter(course => course.category_id === id)
+    res.send(selectedCategories);
+  }
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
